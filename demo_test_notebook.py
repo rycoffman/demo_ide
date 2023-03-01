c52026721636390a3761aa8308e3a3685257b9c6
rycoffman <rycoffman>
2023-03-01 15:28:16 +0000

    confirmed

---
+++ demo_test_notebook.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+print("This is the first cell")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT current_date;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT current_timestamp;
